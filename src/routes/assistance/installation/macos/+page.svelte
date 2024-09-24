c7ab77024221bb602dbd795f79cee20116f5a639
YzRequiem <mxmleosallier@gmail.com>
2024-09-24 09:55:11 +0200

    Change history & New links

--- src/routes/assistance/installation/macos/+page.svelte
+++ src/routes/assistance/installation/macos/+page.svelte
@@ -11,8 +11,8 @@
 		{pdf}
 		title="Installation MacOS"
 		texte="Pour installer le logiciel sur votre ordinateur, suivez les étapes suivantes."
-		downloadLink="https://github.com/Silver-Smok/silverstock-electron-app/releases/download/v1.0.2/SilverStock.dmg"
-		dl2="https://github.com/Silver-Smok/silverstock-electron-app/releases/download/v1.0.2/SilverStock-darwin-x64-1.0.2.zip"
+		downloadLink="https://github.com/Silver-Smok/silverstock-electron-app/releases/download/v1.0.18/SilverStock.dmg"
+		dl2="https://github.com/Silver-Smok/silverstock-electron-app/releases/download/v1.0.18/SilverStock-darwin-x64-1.0.18.zip"
 		textDl1="Pour macOS version 11 ou supérieure"
 		textDl2="Pour macOS version 10 ou inférieure"
 	/>
