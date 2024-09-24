73a9d23171b7c0adf13cfdfa34887b205f88b63c
YzRequiem <mxmleosallier@gmail.com>
2024-09-24 10:07:48 +0200

    Fix text

--- src/routes/assistance/installation/macos/+page.svelte
+++ src/routes/assistance/installation/macos/+page.svelte
@@ -13,7 +13,7 @@
 		texte="Pour installer le logiciel sur votre ordinateur, suivez les étapes suivantes."
 		downloadLink="https://github.com/Silver-Smok/silverstock-electron-app/releases/download/v1.0.18/SilverStock.dmg"
 		dl2="https://github.com/Silver-Smok/silverstock-electron-app/releases/download/v1.0.18/SilverStock-darwin-x64-1.0.18.zip"
-		textDl1="Pour macOS version 11 ou supérieure"
-		textDl2="Pour macOS version 10 ou inférieure"
+		textDl1="Pour macOS version 13 ou supérieure"
+		textDl2="Pour macOS version 12 ou inférieure"
 	/>
 </main>
